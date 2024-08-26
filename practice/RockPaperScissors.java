529accb63acd8892fc1036176785790a07168fac
Griffin <lamb.griffin@icloud.com>
2024-08-26 18:03:21 -0400

    done with test based programming

--- practice/RockPaperScissors.java
+++ practice/RockPaperScissors.java
@@ -6,9 +6,9 @@ public class RockPaperScissors {
     }
 
     public static Results play(Moves move1, Moves move2) {
-        if ((move1 == Moves.ROCK && move2 == Moves.SCISSORS) || (move1 == Moves.SCISSORS && move2 == Moves.PAPER) || (move1 == Moves.PAPER && move2 == Moves.ROCK)) {
+        if (move1 == Moves.PAPER && move2 == Moves.ROCK || move1 == Moves.SCISSORS && move2 == Moves.PAPER || move1 == Moves.ROCK && move2 == Moves.SCISSORS) {
             return Results.PLAYER1;
-        } else if ((move1 == Moves.SCISSORS && move2 == Moves.ROCK) || (move1 == Moves.PAPER && move2 == Moves.SCISSORS) || (move1 == Moves.ROCK && move2 == Moves.PAPER)) {
+        } else if (move1 == Moves.ROCK && move2 == Moves.PAPER || move1 == Moves.PAPER && move2 == Moves.SCISSORS || move1 == Moves.SCISSORS && move2 == Moves.ROCK){
             return Results.PLAYER2;
         } else {
             return Results.TIE;
--- practice/Test.java
+++ practice/Test.java
@@ -4,7 +4,13 @@ public class Test {
     public static void main(String[] args) {
         paperBeatsRock();
         RockBeatsScissors();
+        ScissorsBeatsPaper();
         scissorsLosesRock();
+        paperLosesScissors();
+        rockLosesPaper();
+        rockTie();
+        PaperTie();
+        scissorsTie();
     }
     public static void paperBeatsRock() {
         RockPaperScissors  rockPaperScissors = new RockPaperScissors();
@@ -26,10 +32,70 @@ public class Test {
         }
     }
 
+    public static void ScissorsBeatsPaper() {
+        RockPaperScissors  rockPaperScissors = new RockPaperScissors();
+        Results results = rockPaperScissors.play(Moves.SCISSORS, Moves.PAPER);
+        if (results == Results.PLAYER1) {
+            System.out.println("Test Passes");
+        } else {
+            System.out.println("Test Fails");
+        }
+    }
+
     public static void scissorsLosesRock() {
         RockPaperScissors  rockPaperScissors = new RockPaperScissors();
         Results results = rockPaperScissors.play(Moves.SCISSORS, Moves.ROCK);
         if (results == Results.PLAYER2) {
+            System.out.println("Test Passes");
+        } else {
+            System.out.println("Test Fails");
+        }
+    }
+
+    public static void paperLosesScissors() {
+        RockPaperScissors rockPaperScissors = new RockPaperScissors();
+        Results results = rockPaperScissors.play(Moves.PAPER, Moves.SCISSORS);
+        if (results == Results.PLAYER2) {
+            System.out.println("Test Passes");
+        } else {
+            System.out.println("Test Fails");
+        }
+    }
+
+    public static void rockLosesPaper() {
+        RockPaperScissors rockPaperScissors = new RockPaperScissors();
+        Results results = rockPaperScissors.play(Moves.ROCK, Moves.PAPER);
+        if (results == Results.PLAYER2) {
+            System.out.println("Test Passes");
+        } else {
+            System.out.println("Test Fails");
+        }
+    }
+
+    public static void rockTie() {
+        RockPaperScissors rockPaperScissors = new RockPaperScissors();
+        Results results = rockPaperScissors.play(Moves.ROCK, Moves.ROCK);
+        if (results == Results.TIE) {
+            System.out.println("Test Passes");
+        } else {
+            System.out.println("Test Fails");
+        }
+    }
+
+    public static void PaperTie() {
+        RockPaperScissors rockPaperScissors = new RockPaperScissors();
+        Results results = rockPaperScissors.play(Moves.PAPER, Moves.PAPER);
+        if (results == Results.TIE) {
+            System.out.println("Test Passes");
+        } else {
+            System.out.println("Test Fails");
+        }
+    }
+
+    public static void scissorsTie() {
+        RockPaperScissors rockPaperScissors = new RockPaperScissors();
+        Results results = rockPaperScissors.play(Moves.SCISSORS, Moves.SCISSORS);
+        if (results == Results.TIE) {
             System.out.println("Test Passes");
         } else {
             System.out.println("Test Fails");
